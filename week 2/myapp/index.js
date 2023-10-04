9d31d86d080a61d17d7c25a8c976afe47923c121
Yen-Ting Li <ytli.tw@gmail.com>
2023-10-04 18:22:57 +0800

    remote-week2: add comment and shebang

--- week 2/myapp/index.js
+++ week 2/myapp/index.js
@@ -1,15 +1,21 @@
+#!/usr/bin/env node
 const express = require('express')
+
+// Create a new express app
 const app = express()
 const port = 3000
 
+// Define a route for the healthcheck endpoint
 app.get('/healthcheck', (req, res) => {
   res.send('OK')
 })
 
+// Define a route for the root endpoint
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+// Start the server and listen on the specified port
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
