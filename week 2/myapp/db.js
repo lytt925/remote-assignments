3dc63cf88883cac2ed0689520ab587c483e75246
Yen-Ting Li <ytli.tw@gmail.com>
2023-10-05 18:06:58 +0800

    remote-week2: use dotenv to save db pwd

--- week 2/myapp/db.js
+++ week 2/myapp/db.js
@@ -1,11 +1,11 @@
-// get the client
 const mysql = require('mysql2');
+require('dotenv').config() //config will read your .env file, parse the contents, assign it to process.env,
 
 // create the connection to database
 const connection = mysql.createConnection({
-  host: '',
-  user: '',
-  password: '',
+  host: 'appworks-mysql-1.cwsergwzdswh.us-east-1.rds.amazonaws.com',
+  user: 'admin',
+  password: process.env.MYSQL_PWD,
   database: 'assignment',
   port: '3306'
 });
